7c927c8a5428346bb9b82da2d57c18a2f7ad90c1
nhonnorat <nicolas.honnorat@numericable.fr>
2025-07-31 11:56:48 -0500

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,5 +1,8 @@
-
+# generates cortical maps according to 
+# the best results presented in the publication: Nicolas Honnorat, Mohamad Habes, "*Interpretable Networks to Model the Accumulation of Tau Protein in the Brain*", Workshop on Interpretability of Machine Intelligence in Medical Image Computing (iMIMIC 2025)
 
 python implementation.py -l 5 -d 32 -a 0.001
-
+# 5 layers
+# 32 cortical ROIs (for each brain hemisphere)
+# penalty 0.001
 
